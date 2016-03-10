3727485fb4429b2723668e53314cce38e2cb07c1
Epicodus <mike@epicodus.com>
2016-03-10 13:08:15 -0800

    add api key env variable and promises

---
+++ README.md
@@ -0,0 +1,5 @@
+`.env` file should be added to `.gitignore` and should contain:
+
+```javascript
+exports.apiKey = "API KEY HERE";
+```
--- js/weather-interface.js
+++ js/weather-interface.js
@@ -1,16 +1,18 @@
-var apiKey = "";
+var apiKey = require('../.env').apiKey;
 
 $(document).ready(function(){
   $('#weatherLocation').click(function(){
     var city = $('#location').val();
     $('#location').val("");
-    $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey, function(response) {
+    $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey).then(function(response) {
       console.log(JSON.stringify(response));
       if (response.cod !== 200) {
         $('.showWeather').text(response.message);
       } else {
         $('.showWeather').text("The humidity in " + city + " is " + response.main.humidity + "%");
       }
+    }).fail(function(error) {
+      $('.showWeather').text(error.message);
     });
   });
 });
